5fe18fda9b5879b3de0e512939e61bf7189ae824
kaushikchoudhury <kaushikc44@gmail.com>
2023-11-01 22:50:38 +0530

    issues fixed with the account

--- components/Wallet.jsx
+++ components/Wallet.jsx
@@ -37,22 +37,7 @@ export default function Wallet({children}){
     console.log(wallet)                          /* New */
     }     
 
-    const getAccountBalance = async(accounts) =>{
-        const apiUrl = `https://api.minaexplorer.com/accounts/${accounts}`;
-        const response = await fetch(apiUrl, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json', // Add any headers you need
-            },
-          });
-        if(response.ok){
-            const data = await response.json();
-            console.log(data);
-        }
-        else{
-            console.log("Error");
-        }
-    }
+    
 
     const handleConnect = async  () => {
        

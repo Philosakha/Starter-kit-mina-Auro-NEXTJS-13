f25429680a35dabc1a0d619eaa645b8b6c212b4f
kaushikchoudhury <kaushikc44@gmail.com>
2023-12-18 23:39:36 +0530

    added major commit for balance

--- app/page.js
+++ app/page.js
@@ -3,8 +3,14 @@ import Wallet from "../components/Wallet"
 import Hero from '@/components/HERO'
 
 export default function Home({children}) {
+  if(children.lenght < 0) {throw new Error("The value is not defined")}else{
+    return("value is defined ")
+  }
   return (
     <>
+     <BalanceProvider><Wallet >
+          {children}
+        </Wallet></BalanceProvider>
      <Hero />
     </>
   )
